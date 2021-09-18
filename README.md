dd66d62f9e2f5bf7065f086d205fc2830e558d89
digvijay0899 <78948792+digvijay0899@users.noreply.github.com>
2021-09-18 16:51:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Task_tracker
-Task tracker with list using VueJS
+# task-tracker
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Use fake json server
+while running use npm run backend first then npm run serve on another terminal 
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Task.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
+    <h3>
+      {{task.text}}
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+    </h3>
+    <p>{{task.day}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+  methods: {
+    onDelete(id) {
+      // console.log(id)
+      this.$emit("delete-task", id);
+    },
+    toggleReminder(id) {
+      this.$emit("toggle-reminder", id);
+    },
+  },
+};
+</script>
